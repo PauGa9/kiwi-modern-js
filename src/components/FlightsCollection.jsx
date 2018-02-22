1def92e0f33117b5983bebe0c5c9803fc00455a5
pablito <pablito@pablitos-MacBook-Air.local>
2018-02-22 21:52:48 +0100

    Update the view of FlightsCollection

--- src/components/FlightsCollection.jsx
+++ src/components/FlightsCollection.jsx
@@ -2,11 +2,20 @@ import React from 'react';
 
 export default (props) => {
   const flights = props.flights.map((flight, index) => {
+    const departure = new Date(flight.dTime*1000).toLocaleDateString('en', {weekday: 'short', day: '2-digit', month: 'short', hour: '2-digit', minute: '2-digit'});
+    const arrival = new Date(flight.aTime*1000).toLocaleDateString('en', {weekday: 'short', day: '2-digit', month: 'short', hour: '2-digit', minute: '2-digit'});
     return (
-      <div className="flight" key={index}>
-        <span>from: {flight.from}</span>&nbsp;&nbsp;&nbsp;&nbsp;
-        <span>to: {flight.to}</span>&nbsp;&nbsp;&nbsp;&nbsp;
-        <span>date: {flight.date}</span>&nbsp;&nbsp;&nbsp;&nbsp;
+      <div className="flight" key={index} style={{height: '50px'}}>
+        <div style={{minWidth: '80px', display: 'inline-block'}}>{flight.conversion.EUR} €</div>
+        <div style={{minWidth: '200px', display: 'inline-block', textAlign: 'center'}}>
+          <div>{flight.cityFrom} ({flight.flyFrom})</div>
+          <div>{departure}</div>
+        </div>
+        <div style={{width: '40px', display: 'inline-block', textAlign: 'center'}}>></div>
+        <div style={{minWidth: '200px', display: 'inline-block', textAlign: 'center'}}>
+          <div>{flight.cityTo} ({flight.flyTo})</div>
+          <div>{arrival}</div>
+        </div>
       </div>
     );
   });
