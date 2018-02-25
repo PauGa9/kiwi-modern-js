ce7d51abcde097f205d9ba59bdf978c2e54bd101
pablito <pablito@pablitos-MacBook-Air.local>
2018-02-25 11:51:43 +0100

    Get flights by pages in FlightsCollectionContainer

--- src/components/FlightsCollectionContainer.jsx
+++ src/components/FlightsCollectionContainer.jsx
@@ -7,8 +7,20 @@ class FlightsCollectionContainer extends React.Component {
     super(props);
     this.state = {
       flights: [],
+      page: 0,
+      results: 0
     };
     this.searchFlights = this.searchFlights.bind(this);
+    this.getPrevPage = this.getPrevPage.bind(this);
+    this.getNextPage = this.getNextPage.bind(this);
+  }
+
+  get hasNextPage() {
+    return (this.state.results > 0) && this.state.results > (this.state.page * 5 + 5)
+  }
+
+  get hasPrevPage() {
+    return this.state.page > 0
   }
 
   componentWillReceiveProps(nextProps) {
@@ -17,16 +29,46 @@ class FlightsCollectionContainer extends React.Component {
 
   searchFlights(flightData) {
     APIclient.getFlights(flightData)
-      .then(results => {
+      .then(flights => {
         this.setState({
-          flights: results
+          flights: flights.data,
+          page: 0,
+          results: flights.results
         });
+      });
+  }
+
+  getNextPage() {
+    if (!this.hasNextPage) return;
+
+    APIclient.getFlights(this.props.flightData, this.state.page + 1)
+      .then(results => {
+        this.setState(prevState => ({
+          flights: results.data,
+          page: prevState.page + 1
+        }));
+      });
+  }
+
+  getPrevPage() {
+    if (!this.hasPrevPage) return;
+
+    APIclient.getFlights(this.props.flightData, this.state.page - 1)
+      .then(results => {
+        this.setState(prevState => ({
+          flights: results.data,
+          page: prevState.page - 1
+        }));
       });
   }
 
   render() {
     return (
-      <FlightsCollection flights={this.state.flights} />
+      <div>
+        <FlightsCollection flights={this.state.flights} />
+        <button onClick={this.getPrevPage} disabled={!this.hasPrevPage} >Prev</button>
+        <button onClick={this.getNextPage} disabled={!this.hasNextPage} >Next</button>
+      </div>
     );
   }
 }
